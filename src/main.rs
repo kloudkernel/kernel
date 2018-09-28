8630f863d5b99d5ac649199d36110bcd892f48b1
fred <fredlahde@gmail.com>
2018-09-28 16:05:10 +0200

    vga_buffer: Add test helper

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 #![feature(panic_implementation)]
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+extern crate array_init;
 
 #[macro_use]
 extern crate lazy_static;
@@ -14,12 +21,14 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 #[no_mangle]
+#[cfg(not(test))]
 pub fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
+#[cfg(not(test))]
 pub extern "C" fn _start() -> ! {
     println!("Hello world!");
     loop {}
