a0e46e83432a39c0fb7e3c3294858131ab216963
fred <fredlahde@gmail.com>
2018-09-28 16:38:02 +0200

    add serial crate

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,14 @@ extern crate array_init;
 extern crate lazy_static;
 extern crate bootloader_precompiled;
 extern crate spin;
+extern crate uart_16550;
 extern crate volatile;
+extern crate x86_64;
 
 #[macro_use]
 mod vga_buffer;
+#[macro_use]
+mod serial;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -24,12 +28,33 @@ use core::panic::PanicInfo;
 #[cfg(not(test))]
 pub fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    serial_println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
+#[cfg(not(feature = "integration-test"))]
 #[cfg(not(test))]
 pub extern "C" fn _start() -> ! {
     println!("Hello world!");
+    serial_println!("Hello world!");
     loop {}
+}
+
+#[no_mangle]
+#[cfg(feature = "integration-test")]
+#[cfg(not(test))]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+    serial_println!("Hello world!");
+    exit_qemu();
+    loop {}
+}
+
+pub fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+    let mut port = Port::<u32>::new(0xf4);
+    unsafe {
+        port.write(0);
+    }
 }
