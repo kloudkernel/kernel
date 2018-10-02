d8e3a76e2300dfee8f5a8d18798831fea471297f
fred <fredlahde@gmail.com>
2018-10-02 12:40:23 +0200

    interrupt: macros to quickly create handler

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,5 +1,29 @@
 use gdt;
-use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable};
+use x86_64::structures::idt::{ExceptionStackFrame, InterruptDescriptorTable, PageFaultErrorCode};
+
+macro_rules! handler_with_error_code {
+    ($name: ident, $msg: expr) => {
+        extern "x86-interrupt" fn $name(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
+            let msg = $msg;
+            println!("{}: {:#?}\n Error Code: {}", msg, stack_frame, error_code);
+            serial_println!("{}: {:#?}\n Error Code: {}", msg, stack_frame, error_code);
+
+            loop {}
+        }
+    };
+}
+
+macro_rules! handler {
+    ($name: ident, $msg: expr) => {
+        extern "x86-interrupt" fn $name(stack_frame: &mut ExceptionStackFrame) {
+            let msg = $msg;
+            println!("{}: {:#?}\n", msg, stack_frame);
+            serial_println!("{}: {:#?}\n", msg, stack_frame);
+
+            loop {}
+        }
+    };
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -10,31 +34,36 @@ lazy_static! {
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
         }
 
+        idt.general_protection_fault.set_handler_fn(general_protection_handler);
+        idt.alignment_check.set_handler_fn(alingment_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
     };
 }
+handler_with_error_code!(alingment_handler, "ALIGNEMNT FAULT");
+handler_with_error_code!(double_fault_handler, "DOUBLE FAULT");
+handler_with_error_code!(general_protection_handler, "GENERAL PROTECTION FAULT");
+
+handler!(breakpoint_handler, "BREAKPOINT");
 
 pub fn init_idt() {
     IDT.load();
 }
 
-extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
-    println!("BREAKPOINT: {:#?}", stack_frame);
-    serial_println!("BREAKPOINT: {:#?}", stack_frame);
-}
-
-extern "x86-interrupt" fn double_fault_handler(
+extern "x86-interrupt" fn page_fault_handler(
     stack_frame: &mut ExceptionStackFrame,
-    error_code: u64,
+    error_code: PageFaultErrorCode,
 ) {
     println!(
-        "DOUBLE FAULT: {:#?}\n Error Code: {}",
+        "PAGE FAULT: {:#?}\n Error Code: {:#?}",
         stack_frame, error_code
     );
     serial_println!(
-        "DOUBLE FAULT: {:#?}\n Error Code: {}",
+        "PAGE_FAULT: {:#?}\n Error Code: {:#?}",
         stack_frame,
         error_code
     );
+
     loop {}
 }
